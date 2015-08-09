1a881142c11012d978e474933c47fc90b3f8fd93
akievet <akievet@gmail.com>
2015-08-09 17:47:03 -0400

    - Add placeholder blog page - Tweek spacing between sections

--- js/main.js
+++ js/main.js
@@ -4,8 +4,22 @@ var delayFadeIn = function($elem, time) {
 	})
 }
 
+
 $(document).ready(function(){
 	delayFadeIn($('.developer'), 1000);
 	delayFadeIn($('.designer'), 1500);
 	delayFadeIn($('.adventurer'), 2000);
+
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
